74d2e697c11fd433b69d0e475082ffc76bf70391
GestorEterno <eternalgenesisgestor@gmail.com>
2025-05-31 16:53:30 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -22,12 +22,3 @@ console.log("🔥 Firebase inicializado correctamente (PRIVATE)");
 
 export { app, db, auth };
 
-{
-  "titulo": "Introducción a la escritura narrativa",
-  "descripcion": "Curso básico para aprender storytelling",
-  "categoria": "Escritura",
-  "tipo": "video", // puede ser 'video', 'audio', 'libro'
-  "enlace": "https://url-del-recurso.mp4",
-  "fecha": "timestamp"
-}
-
